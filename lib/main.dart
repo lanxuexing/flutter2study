6af00f0adeb17f355cb1a20f14c818ce77265beb
兰学兴 <lanxuexing@xdf.cn>
2019-02-02 16:15:58 +0800

    Container Widget

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,41 @@
 /**
  * 2.Text Widget
  */
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Text Widget Demo',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Welcome to Flutter'),
+//         ),
+//         body: Center(
+//           child: Text(
+//             '新华社北京2月1日电  中华民族传统节日农历春节来临之际，中共中央总书记、国家主席、中央军委主席习近平在北京看望慰问基层干部群众，考察北京冬奥会、冬残奥会筹办工作，向全国各族人民致以美好的新春祝福，祝各族人民幸福安康，祝伟大祖国繁荣吉祥。',
+//             textAlign: TextAlign.justify,
+//             maxLines: 3,
+//             overflow: TextOverflow.ellipsis, // 设置文字溢出...表示
+//             style: TextStyle(
+//               color: Color.fromARGB(255, 255, 125, 125),
+//               decoration: TextDecoration.underline,
+//               decorationColor: Color(0xff2b98f0),
+//               fontSize: 14.0
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+/**
+ * 3.Container Widget
+ */
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -33,24 +68,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Text Widget Demo',
+      title: 'Flutter Container Widget Demo',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome to Flutter'),
         ),
-        body: Center(
-          child: Text(
-            '新华社北京2月1日电  中华民族传统节日农历春节来临之际，中共中央总书记、国家主席、中央军委主席习近平在北京看望慰问基层干部群众，考察北京冬奥会、冬残奥会筹办工作，向全国各族人民致以美好的新春祝福，祝各族人民幸福安康，祝伟大祖国繁荣吉祥。',
-            textAlign: TextAlign.justify,
-            maxLines: 3,
-            overflow: TextOverflow.ellipsis, // 设置文字溢出...表示
-            style: TextStyle(
-              color: Color.fromARGB(255, 255, 125, 125),
-              decoration: TextDecoration.underline,
-              decorationColor: Color(0xff2b98f0),
-              fontSize: 14.0
-            ),
-          ),
+        body: Container(
+          child: Text('新华社北京2月1日电  中华民族传统节日农历春节来临之际，中共中央总书记、国家主席、中央军委主席习近平在北京看望慰问基层干部群众，考察北京冬奥会、冬残奥会筹办工作，向全国各族人民致以美好的新春祝福，祝各族人民幸福安康，祝伟大祖国繁荣吉祥。'),
+          alignment: Alignment.center,
+          color: Color(0xffc9c9c9),
+          width: 400.0,
+          height: 200.0,
+          margin: EdgeInsets.all(20.0),
+          padding: EdgeInsets.all(10.0),
         ),
       ),
     );
